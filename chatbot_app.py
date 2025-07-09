6d17f6d68682bc3e7bc5540bd2a5519bd51e1c35
NinetteIninahazwe003 <ninette.ininah@gmail.com>
2025-07-09 11:47:51 +0200

    Update chatbot_app.py

--- chatbot_app.py
+++ chatbot_app.py
@@ -10,7 +10,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 # Configure Gemini
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel('gemini-pro')
+model = genai.GenerativeModel(models/gemini-1.5-flash-latest)
 
 # Title
 st.title("🩺 Ninette Medical Assistant")
